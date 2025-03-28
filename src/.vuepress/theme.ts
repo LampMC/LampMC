4e2f3e5d29d737530b0eafdbf813c957c9e664dd
liangcha385 <billbill_lcssg@163.com>
2025-03-28 21:22:21 +0800

    评论插件配置

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -122,10 +122,10 @@ export default hopeTheme({
     // 注意: 仅用于测试! 你必须自行生成并在生产环境中使用自己的评论服务
     comment: {
       provider: "Giscus",
-      repo: "vuepress-theme-hope/giscus-discussions",
-      repoId: "R_kgDOG_Pt2A",
+      repo: "LampMC/LampMC",
+      repoId: "R_kgDOOQOslA",
       category: "Announcements",
-      categoryId: "DIC_kwDOG_Pt2M4COD69",
+      categoryId: "DIC_kwDOOQOslM4Coi_Y",
     },
 
     components: {
